d74642f10ab196d478da19c66bc37df451e99a79
Mark Taylor <m.b.taylor@bristol.ac.uk>
2026-03-31 12:33:16 +0100

    dr3rvs: get full spectrum file from ESAC bulk data download

--- dr3rvs/download-ecsvs.sh
+++ dr3rvs/download-ecsvs.sh
@@ -2,7 +2,7 @@
 datadir=rvs_mean_spectrum
 tmpfile=
 mkdir -p $datadir
-rm $datadir/part-*
+rm -f $datadir/part-*
 while read f
 do
    if [ -r $datadir/$f ]
@@ -13,4 +13,4 @@ do
       curl -s https://cdn.gea.esac.esa.int/Gaia/gdr3/Spectroscopy/rvs_mean_spectrum/$f > $datadir/part-$f && \
       mv $datadir/part-$f $datadir/$f
    fi
-done < ecsv-names.txt
+done < ecsv-names.lis
